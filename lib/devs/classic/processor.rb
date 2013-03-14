f80039f136dd5be95c65729639563a855e0e13f6
Romain Franceschini <franceschini.romain@gmail.com>
2013-03-14 11:54:54 +0100

    removed ugly aliases

--- lib/devs/classic/processor.rb
+++ lib/devs/classic/processor.rb
@@ -5,9 +5,6 @@ module DEVS
       attr_accessor :parent
       attr_reader :model, :time_next, :time_last
 
-      alias_method :tn, :time_next
-      alias_method :tl, :time_last
-
       def initialize(model)
         @model = model
         @time_next = 0
