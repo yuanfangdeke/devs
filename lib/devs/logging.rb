90dd3e5a01a32fc1ae138ad02b76adb7a894c552
Romain Franceschini <franceschini.romain@gmail.com>
2013-06-13 16:45:34 +0200

    use nullobject pattern for default logger

--- lib/devs/logging.rb
+++ lib/devs/logging.rb
@@ -1,36 +1,44 @@
 module DEVS
+  class NullLogger
+    def debug(s); end
+    def info(s); end
+    def warn(s); end
+    def error(s); end
+    def fatal(s); end
+  end
+
   class << self
     attr_accessor :logger
   end
-  @logger = Logger.new(STDOUT)
+  @logger = NullLogger.new
 
   module Logging
     # Send a debug message
     #
     # @param string [String] the string to log
     def debug(string)
-      DEVS.logger.debug(string) if DEVS.logger
+      DEVS.logger.debug(string)
     end
 
     # Send a info message
     #
     # @param string [String] the string to log
     def info(string)
-      DEVS.logger.info(string) if DEVS.logger
+      DEVS.logger.info(string)
     end
 
     # Send a warning message
     #
     # @param string [String] the string to log
     def warn(string)
-      DEVS.logger.warn(string) if DEVS.logger
+      DEVS.logger.warn(string)
     end
 
     # Send an error message
     #
     # @param string [String] the string to log
     def error(string)
-      DEVS.logger.error(string) if DEVS.logger
+      DEVS.logger.error(string)
     end
 
     module_function :debug, :info, :warn, :error
