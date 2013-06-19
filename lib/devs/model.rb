yuanfangdeke/devs

commit c9d05e38b45f9eb95a6f8f0e1847fe7e5aa5610b

allows to add several ports with one method call

lib/devs/model.rb
@@ -28,21 +28,23 @@ module DEVS
     # Adds an input port to <i>self</i>.
     #
     # @param name [String, Symbol]
-    # @return [Port] the newly created port
-    def add_input_port(name = "input_port_#{self.input_ports.size}")
-      port = Port.new(self, :input, name)
-      @input_ports << port
-      port
+    # @return [nil]
+    def add_input_port(*names)
+      names.each do |name|
+        @input_ports << Port.new(self, :input, name)
+      end
+      nil
     end
 
     # Adds an output port to <i>self</i>.
     #
     # @param name [String, Symbol] the port name
-    # @return [Port] the newly created port
-    def add_output_port(name = "output_port_#{self.output_ports.size}")
-      port = Port.new(self, :output, name)
-      @output_ports << port
-      port
+    # @return [nil]
+    def add_output_port(*names)
+      names.each do |name|
+        @output_ports << Port.new(self, :output, name)
+      end
+      nil
     end
 
     # Returns the list of input ports' names

commit 25819abce9be5f6c3fbb5bbbd5aed07561b3ccd4

fixed a regression caused by c9d05e38b45f9eb95a6f8f0e1847fe7e5aa5610b

lib/devs/model.rb
@@ -28,23 +28,31 @@ module DEVS
     # Adds an input port to <i>self</i>.
     #
     # @param name [String, Symbol]
-    # @return [nil]
+    # @return [Port, Array<Port>] the created port or the list of created ports
     def add_input_port(*names)
-      names.each do |name|
-        @input_ports << Port.new(self, :input, name)
+      ports = names.map { |name| Port.new(self, :input, name) }
+      @input_ports.concat(ports)
+      
+      if ports.size == 1
+        ports.first
+      else
+        ports
       end
-      nil
     end
 
     # Adds an output port to <i>self</i>.
     #
     # @param name [String, Symbol] the port name
-    # @return [nil]
+    # @return [Port, Array<Port>] the created port or the list of created ports
     def add_output_port(*names)
-      names.each do |name|
-        @output_ports << Port.new(self, :output, name)
+      ports = names.map { |name| Port.new(self, :output, name) }
+      @output_ports.concat(ports)
+      
+      if ports.size == 1
+        ports.first
+      else
+        ports
       end
-      nil
     end
 
     # Returns the list of input ports' names