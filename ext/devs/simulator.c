409108c69334c096b623e02a4f7503cb0af7858b
Romain Franceschini <franceschini.romain@gmail.com>
2013-07-24 17:04:44 +0200

    fixed dispatch method

--- ext/devs/simulator.c
+++ ext/devs/simulator.c
@@ -20,31 +20,30 @@ init_devs_simulator() {
 static VALUE
 dispatch(VALUE self, VALUE event) {
     VALUE result = Qnil;
-    VALUE type = rb_ivar_get(event, rb_intern("@type"));
-    VALUE hsh = rb_ivar_get(self, rb_intern("@events_count"));
-    VALUE model = rb_ivar_get(self, rb_intern("@model"));
+    VALUE type = rb_iv_get(event, "@type");
+    VALUE hsh = rb_iv_get(self, "@events_count");
+    VALUE model = rb_iv_get(self, "@model");
     int count = FIX2INT(rb_hash_aref(hsh, type));
-    VALUE str = rb_str_new2(StringValuePtr(model));
-    rb_str_cat2(str, " received ");
-    rb_str_cat2(str, StringValuePtr(event));
+
+    // VALUE str = rb_str_new2(RSTRING_PTR(rb_any_to_s(model)));
+    // rb_str_cat2(str, " received ");
+    // rb_str_cat2(str, RSTRING_PTR(rb_any_to_s(event)));
 
     rb_hash_aset(hsh, type, INT2FIX(count + 1));
-    rb_funcall(self, rb_intern("debug"), str);
+//    rb_funcall(self, rb_intern("debug"), str);
 
-    char m[80];
-    strcat(m, "handle_");
-    strcat(m, RSTRING_PTR(type));
-    strcat(m, "_event");
+    VALUE m = rb_str_new2("handle_");
+    rb_str_cat2(m, rb_id2name(SYM2ID(type)));
+    rb_str_cat2(m, "_event");
 
-    ID handler = rb_intern(m);
-
+    ID handler = rb_intern(RSTRING_PTR(m));
     if (rb_respond_to(self, handler)) {
         result = rb_funcall(self, handler, 1, event);
     } else {
         rb_raise(
             rb_eRuntimeError,
             "simulator doesn't handle %s events",
-            RSTRING_PTR(m)
+            RSTRING_PTR(rb_any_to_s(type))
         );
     }
 
