389b159a703dad1c1b9eabb6763418a13268931a
Romain Franceschini <franceschini.romain@gmail.com>
2013-07-24 17:03:51 +0200

    implemented custom c debug method

--- ext/devs/devs.c
+++ ext/devs/devs.c
@@ -4,6 +4,20 @@ VALUE mDEVS;
 VALUE mDEVSClassic;
 
 void
+devs_debug(const char *file, int lines, char *fmt, ...) {
+    if (strlen(fmt) < 255) {
+        char buffer[255];
+        va_list arg_ptr;
+
+        va_start(arg_ptr, fmt);
+        vsprintf(buffer, fmt, arg_ptr);
+        va_end(arg_ptr);
+
+        fprintf(stdout, "devs-ext: %s:%d - %s\n", file, lines, buffer);
+    }
+}
+
+void
 Init_devs() {
     mDEVS = rb_define_module("DEVS");
     mDEVSClassic = rb_define_module_under(mDEVS, "Classic");
--- ext/devs/devs.h
+++ ext/devs/devs.h
@@ -1,11 +1,40 @@
 #ifndef DEVS_NATIVE
 #define DEVS_NATIVE
 
+#include <stdlib.h>
+#include <string.h>
+
 #include <ruby.h>
 
 #include <simulator.h>
 
 extern VALUE mDEVS;
 extern VALUE mDEVSClassic;
+
+void devs_debug(const char *file, int lines, char *fmt, ...);
+
+#define DEBUG
+
+#ifdef DEBUG
+#define DEVS_DEBUG(fmt...) devs_debug(__FILE__, __LINE__, fmt);
+#else
+#define DEVS_DEBUG(p)
+#endif
+
+#ifndef RSTRING_PTR
+#define RSTRING_PTR(s) (RSTRING(s)->ptr)
+#endif
+
+#ifndef RSTRING_LEN
+#define RSTRING_LEN(s) (RSTRING(s)->len)
+#endif
+
+#ifndef RARRAY_PTR
+#define RARRAY_PTR(a) RARRAY(a)->ptr
+#endif
+
+#ifndef RARRAY_LEN
+#define RARRAY_LEN(a) RARRAY(a)->len
+#endif
 
 #endif
