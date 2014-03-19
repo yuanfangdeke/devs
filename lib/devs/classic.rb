7c10995c46ba61d84c3d87fbf08f315dd0bc0061
Romain Franceschini <franceschini.romain@gmail.com>
2014-03-19 15:48:12 +0100

    should have been part of commit eef997e597d63fdd04a63f60a75c90fcbc92cd29

--- lib/devs/classic.rb
+++ lib/devs/classic.rb
@@ -1,10 +1,3 @@
 require 'devs/classic/simulator_impl'
 require 'devs/classic/coordinator_impl'
 require 'devs/classic/root_coordinator_strategy'
-
-module DEVS
-  def simulate(&block)
-    Builders::SimulationBuilder.new(Classic, &block).root_coordinator.simulate
-  end
-  module_function :simulate
-end
--- lib/devs/parallel.rb
+++ lib/devs/parallel.rb
@@ -1,18 +1,3 @@
 require 'devs/parallel/simulator_impl'
 require 'devs/parallel/coordinator_impl'
 require 'devs/parallel/root_coordinator_strategy'
-
-module DEVS
-  # Runs a simulation
-  # @todo
-  # @example
-  #   simulate do
-  #     duration = 200
-  #
-  #   end
-  #
-  def simulate(&block)
-    Builders::SimulationBuilder.new(Parallel, &block).root_coordinator.simulate
-  end
-  module_function :simulate
-end
