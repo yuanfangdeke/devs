b6755fd28fe788c686613b47694a71c45fd6561e
Romain Franceschini <franceschini.romain@gmail.com>
2014-05-27 16:16:55 +0200

    removed pqueue dependency

--- lib/devs/scheduler.rb
+++ lib/devs/scheduler.rb
@@ -1,68 +1,179 @@
-class PQueue
-  def delete(v)
-    i = @que.size - 1
-    elmt = nil
-    while i >= 0
-      if @que[i] == v
-        elmt = @que.delete_at(i)
-        break
-      end
-      i -= 1
-    end
-    elmt
-  end
-
-  def read_while
-    return nil unless block_given? || empty?
-    ary = []
-    i = @que.size - 1
-
-    loop do
-      elt = @que[i]
-      if yield(elt)
-        ary << elt
-        break if i.zero?
-        i -= 1
-      else
-        break
-      end
-    end
-
-    ary
-  end
-end
-
 module DEVS
-  class Scheduler
-    def initialize(children = nil)
-      @pq = PQueue.new(children) { |a, b| b.time_next <=> a.time_next }
+  class BinaryHeapScheduler
+    def initialize(elements = nil)
+      @que = []
+      replace(elements) if elements
+    end
+
+    protected
+    attr_reader :que
+
+    public
+
+    def size
+      @que.size
     end
 
     def schedule(model)
-      @pq.push(model)
+      @que << model
+      reheap(@que.size - 1)
+      self
     end
 
+    def reschedule(model)
+      idx = index(model)
+      reheap(idx) unless idx.nil?
+      self
+    end
+
+    def index(model)
+      idx = nil
+      i = @que.size - 1
+      while i >= 0
+        if @que[i] == model
+          idx = i
+          break
+        end
+        i -= 1
+      end
+      idx
+    end
+    private :index
+
     def unschedule(model)
-      @pq.delete(model)
+      i = @que.size - 1
+      elmt = nil
+      while i >= 0
+        if @que[i] == model
+          elmt = @que.delete_at(i)
+          break
+        end
+        i -= 1
+      end
+      elmt
     end
 
     def read
-      top = @pq.top
-      top.time_next if top
-    end
-
-    def reschedule
-      @pq.send :sort!
-    end
-
-    def read_imminent(time)
-      @pq.read_while { |m| m.time_next == time }
+      return nil if empty?
+      @que.last.time_next
     end
 
     def imminent(time)
-      children = []
-      children << @pq.pop while @pq.top.time_next == time
-      children
+      a = []
+      a << @que.pop while @que.last.time_next == time
+      a
+    end
+
+    def read_imminent(time)
+      ary = []
+      i = @que.size - 1
+
+      while i >= 0
+        elt = @que[i]
+        if elt.time_next == time
+          ary << elt
+          i -= 1
+        else
+          break
+        end
+      end
+
+      ary
+    end
+
+    def empty?
+      @que.empty?
+    end
+
+    def to_a
+      @que.dup
+    end
+
+    def reschedule!
+      @que.sort! { |a,b| b.time_next <=> a.time_next }
+      self
+    end
+
+    def concat(elements)
+      if empty?
+        if elements.kind_of?(PQueue)
+          initialize_copy(elements)
+        else
+          replace(elements)
+        end
+      else
+        if elements.kind_of?(PQueue)
+          @que.concat(elements.que)
+          reschedule!
+        else
+          @que.concat(elements.to_a)
+          reschedule!
+        end
+      end
+      return self
+    end
+
+    def replace(elements)
+      if elements.kind_of?(PQueue)
+        initialize_copy(elements)
+      else
+        @que.replace(elements.to_a)
+        reschedule!
+      end
+      self
+    end
+
+    def inspect
+      "<#{self.class}: size=#{size}, top=#{top || "nil"}>"
+    end
+
+    def ==(other)
+      size == other.size && to_a == other.to_a
+    end
+
+    private
+
+    def initialize_copy(other)
+      @que  = other.que.dup
+      reschedule!
+    end
+
+    def reheap(k)
+      return self if size <= 1
+
+      que = @que.dup
+
+      v = que.delete_at(k)
+      i = binary_index(que, v)
+
+      que.insert(i, v)
+
+      @que = que
+
+      return self
+    end
+
+    def binary_index(que, target)
+      upper = que.size - 1
+      lower = 0
+
+      while(upper >= lower) do
+        idx  = lower + (upper - lower) / 2
+        comp = que[idx].time_next <=> target.time_next
+
+        case comp
+        when 0, nil
+          return idx
+        when 1, true
+          lower = idx + 1
+        when -1, false
+          upper = idx - 1
+        else
+        end
+      end
+      lower
     end
   end
+
+  Scheduler = BinaryHeapScheduler
 end
