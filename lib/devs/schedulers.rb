29a71cd61486bfc04597b7b1775382387b6006aa
Romain Franceschini <franceschini.romain@gmail.com>
2014-06-02 10:53:34 +0200

    sorted list as a default scheduler

--- lib/devs/schedulers.rb
+++ lib/devs/schedulers.rb
@@ -2,4 +2,6 @@ require 'devs/schedulers/binary_heap_scheduler'
 require 'devs/schedulers/sorted_list_scheduler'
 require 'devs/schedulers/minimal_list_scheduler'
 
-Scheduler = MinimalListScheduler
+module DEVS
+  Scheduler = SortedListScheduler
+end
