ab51de62f7b00283aadc5b0c2ca1ff2120d8b7b4
Romain Franceschini <franceschini.romain@gmail.com>
2014-07-02 14:43:04 +0200

    optimized minimal list scheduler

--- lib/devs/schedulers/minimal_list_scheduler.rb
+++ lib/devs/schedulers/minimal_list_scheduler.rb
@@ -13,16 +13,17 @@ module DEVS
 
     def unschedule(processor)
       @ary.delete(processor)
-      if processor.time_next == @min
-        min = DEVS::INFINITY
-        @ary.each { |p| min = p.time_next if p.time_next < min }
-        @min = min
-      end
+      search_min! if processor.time_next == @min
     end
 
     def reschedule!
       min = DEVS::INFINITY
-      @ary.each { |p| min = p.time_next if p.time_next < min }
+      i = 0
+      while i < @ary.size
+        p = @ary[i]
+        min = p.time_next if p.time_next < min
+        i += 1
+      end
       @min = min
     end
 
@@ -30,11 +31,26 @@ module DEVS
       @min
     end
 
-    def imminent(time)
+    def read_imminent(time)
       a = []
-      @ary.each { |p| a.push(p) if p.time_next == time }
+      i = 0
+      while i < @ary.size
+        p = @ary[i]
+        a << p if p.time_next == time
+        i += 1
+      end
       a
     end
-    alias_method :read_imminent, :imminent
+
+    def imminent(time)
+      a = []
+      i = 0
+      while i < @ary.size
+        p = @ary[i]
+        a << @ary.delete_at(i) if p.time_next == time
+        i += 1
+      end
+      a
+    end
   end
 end
