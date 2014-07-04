4ed1aa02852849f0f59b8f503475ca80b08d788f
Romain Franceschini <franceschini.romain@gmail.com>
2014-07-04 16:41:59 +0200

    user simulation interface made through a new Simulation object

--- lib/devs/parallel.rb
+++ lib/devs/parallel.rb
@@ -4,4 +4,4 @@ require 'devs/parallel/root_coordinator_strategy'
 
 require 'devs/parallel/sequential_simulator_impl'
 require 'devs/parallel/sequential_coordinator_impl'
-require 'devs/parallel/sequential_root_coordinator_strategy'
+require 'devs/parallel/sequential_simulable'
--- lib/devs/simulation.rb
+++ lib/devs/simulation.rb
@@ -1,18 +1,12 @@
 module DEVS
-  # This class represent the processor on top of the simulation tree,
-  # responsible for coordinating the simulation
-  class RootCoordinator
-    # used for hooks
-    include Observable
+  # This class represent the interface to the simulation
+  class Simulation
+    include Observable        # used for hooks
     include Logging
+    include Enumerable
 
-    attr_accessor :duration
-
-    # The default duration of the simulation if argument omitted
-    DEFAULT_DURATION = 60
-
-    attr_reader :duration, :start_time, :final_time, :child
-    attr_accessor :time, :init_time
+    attr_reader :duration, :start_time, :final_time, :time, :processor,
+                :build_start_time, :build_end_time, :build_elapsed_secs
 
     # @!attribute [r] time
     #   @return [Numeric] Returns the current simulation time
@@ -20,37 +14,34 @@ module DEVS
     # @!attribute [r] start_time
     #   @return [Time] Returns the time at which the simulation started
 
-    # @!attribute [r] duration
+    # @!attribute [rw] duration
     #   @return [Numeric] Returns the total duration of the simulation time
 
-    # @!attribute [rw] init_time
+    # @!attribute [r] init_time
     #   @return [Numeric] Returns the duration of the initialization
 
-    # Returns a new {RootCoordinator} instance.
+    # Returns a new {Simulation} instance.
     #
     # @param child [Coordinator] the child coordinator
     # @param strategy [Module] the strategy responding ro run
     # @param duration [Numeric] the duration of the simulation
     # @raise [ArgumentError] if the child is not a coordinator
-    def initialize(child, strategy, duration = DEFAULT_DURATION)
-      unless child.is_a?(Coordinator)
-        raise ArgumentError, 'child must be of Coordinator type'
-      end
-      @strategy = strategy
+    def initialize(processor, duration, build_start_time)
       @duration = duration
       @time = 0
-      @child = child
+      @processor = processor
       @lock = Mutex.new
+      hooks.each { |observer| add_observer(observer) }
+      @build_start_time = build_start_time
+      @build_end_time = Time.now
+      @build_elapsed_secs = @build_end_time - @build_start_time
+      info "*** Builded simulation at #{@build_end_time} after #{@build_elapsed_secs} secs." if DEVS.logger
     end
 
     def time
       @lock.synchronize { @time }
     end
     alias_method :clock, :time
-
-    def time=(v)
-      @lock.synchronize { @time = v }
-    end
 
     def duration=(v)
       @duration = v if waiting?
@@ -132,7 +123,7 @@ module DEVS
     # @return [Hash<Symbol, Fixnum>]
     def stats
       if done?
-        @stats ||= (stats = @child.stats
+        @stats ||= (stats = @processor.stats
           total = Hash.new(0)
           stats.values.each { |h| h.each { |k, v| total[k] += v if v }}
           stats[:TOTAL] = total
@@ -144,27 +135,15 @@ module DEVS
     # Run the simulation in a new thread
     def simulate
       if waiting?
-        @lock.synchronize do
-          @start_time = Time.now
-          info "*** Beginning simulation at #{@start_time} with duration: #{@duration}" if DEVS.logger
+        begin_simulation
+        self.time = t = @processor.init(self.time)
+        run = true
+        while run
+          "* Tick at: #{time}, #{Time.now - @start_time} secs elapsed" if DEVS.logger
+          self.time = t = @processor.step(t)
+          run = false if t >= @duration
         end
-
-        # Calling the core algorithm
-        @strategy.run(self)
-
-        final_time = Time.now
-        @lock.synchronize { @final_time = final_time }
-
-        if DEVS.logger
-          info "*** Simulation ended at #{final_time} after #{elapsed_secs} secs."
-          info "* Events stats : {"
-          stats.each { |k, v| info "    #{k} => #{v}" }
-          info "* }"
-          info "* Calling post simulation hooks"
-        end
-
-        changed
-        notify_observers(:post_simulation)
+        end_simulation
       else
         if running?
           error "The simulation already started at #{@start_time} and is currently running."
@@ -172,8 +151,82 @@ module DEVS
           error "The simulation is already done. Started at #{@start_time} and finished at #{@final_time} in #{elapsed_secs} secs."
         end if DEVS.logger
       end
-
       self
+    end
+
+    def each(&block)
+      if waiting?
+        if block_given?
+          begin_simulation
+          self.time = t = @processor.init(self.time)
+          run = true
+          while run
+            "* Tick at: #{time}, #{Time.now - @start_time} secs elapsed" if DEVS.logger
+            self.time = t = @processor.step(t)
+            yield(self, t)
+            run = false if t >= @duration
+          end
+          end_simulation
+        else
+          return enum_for(:each, &block)
+        end
+      elsif DEVS.logger
+        if running?
+          error "The simulation already started at #{@start_time} and is currently running."
+        else
+          error "The simulation is already done. Started at #{@start_time} and finished at #{@final_time} in #{elapsed_secs} secs."
+        end
+        nil
+      end
+    end
+
+    private
+    def time=(v)
+      @lock.synchronize { @time = v }
+    end
+
+    def hooks
+      models = Array.new(@processor.model.components)
+      observers = []
+      index = 0
+
+      while index < models.count
+        model = models[index]
+
+        if model.atomic? && model.observer?
+          observers << model
+        elsif model.coupled?
+          models.concat(model.components)
+        end
+
+        index += 1
+      end
+
+      observers
+    end
+    private :hooks
+
+    def begin_simulation
+      @lock.synchronize do
+        @start_time = Time.now
+        info "*** Beginning simulation at #{@start_time} with duration: #{@duration}" if DEVS.logger
+      end
+    end
+
+    def end_simulation
+      final_time = Time.now
+      @lock.synchronize { @final_time = final_time }
+
+      if DEVS.logger
+        info "*** Simulation ended at #{final_time} after #{elapsed_secs} secs."
+        debug "* Events stats : {"
+        stats.each { |k, v| debug "    #{k} => #{v}" }
+        debug "* }"
+        debug "* Calling post simulation hooks"
+      end
+
+      changed
+      notify_observers(:post_simulation)
     end
   end
 end
