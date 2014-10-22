632cc5cd0c98b298ac48dd78ce844908fcde9014
Romain Franceschini <franceschini.romain@gmail.com>
2014-10-22 14:32:53 +0200

    reuse Ev class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,11 @@ require 'pry-nav'
 require 'pry-stack_explorer'
 require 'codeclimate-test-reporter'
 
+class Ev
+  attr_accessor :time_next
+  def initialize(tn)
+    @time_next = tn
+  end
+end
+
 CodeClimate::TestReporter.start
